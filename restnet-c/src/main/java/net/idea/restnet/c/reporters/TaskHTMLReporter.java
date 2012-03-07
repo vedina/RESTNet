90a52f5ea32ec76dfaf1f178e7f1c910091e5fa7
ngn <jeliazkova.nina@gmail.com>
2012-03-08 00:11:54 +0200

    headless option and separate navigation bar

--- restnet-c/src/main/java/net/idea/restnet/c/reporters/TaskHTMLReporter.java
+++ restnet-c/src/main/java/net/idea/restnet/c/reporters/TaskHTMLReporter.java
@@ -17,6 +17,15 @@ import net.idea.restnet.i.task.TaskResult;
 import org.restlet.Request;
 
 public class TaskHTMLReporter<USERID> extends CatalogURIReporter<UUID> {
+	protected boolean headless = false;
+	public boolean isHeadless() {
+		return headless;
+	}
+
+
+	public void setHeadless(boolean headless) {
+		this.headless = headless;
+	}
 	protected ITaskStorage<USERID> storage;
 	protected HTMLBeauty htmlBeauty;
 	public HTMLBeauty getHtmlBeauty() {
@@ -43,26 +52,29 @@ public class TaskHTMLReporter<USERID> extends CatalogURIReporter<UUID> {
 
 	@Override
 	public void header(Writer output, Iterator<UUID> query) {
+		if (headless) return;
 		try {
-			
-			
-			String max = getRequest().getResourceRef().getQueryAsForm().getFirstValue(AbstractResource.max_hits);
-			max = max==null?"10":max;
-			
 			if (htmlBeauty==null) htmlBeauty = new HTMLBeauty();
-			htmlBeauty.writeHTMLHeader(output, htmlBeauty.getTitle(), getRequest(),"",
+			htmlBeauty.writeHTMLHeader(output, htmlBeauty.getTitle(), getRequest(),"<meta http-equiv=\"refresh\" content=\"10\">",
 					getDocumentation()
 					);//,"<meta http-equiv=\"refresh\" content=\"10\">");
-			output.write("<h4>Tasks:");
-			for (TaskStatus status :TaskStatus.values())
-				output.write(String.format("<a href='%s%s?search=%s&%s=%s'>%s</a>&nbsp;",
-						baseReference,SimpleTaskResource.resource,status,AbstractResource.max_hits,max,status));
-			output.write("</h4><p>");
+			
+			printNavigation();
 			output.write("<table>");
 			output.write("<tr><th>Start time</th><th>Elapsed time,ms</th><th>Task</th><th>Name</th><th colspan='2'>Status</th><th></th></tr>");
 		} catch (Exception x) {
 			
 		}
+	}
+	
+	protected void printNavigation() throws Exception {
+		String max = getRequest().getResourceRef().getQueryAsForm().getFirstValue(AbstractResource.max_hits);
+		max = max==null?"10":max;
+		output.write("<h4>Tasks:");
+		for (TaskStatus status :TaskStatus.values())
+			output.write(String.format("<a href='%s%s?search=%s&%s=%s'>%s</a>&nbsp;",
+					baseReference,SimpleTaskResource.resource,status,AbstractResource.max_hits,max,status));
+		output.write("</h4><p>");
 	}
 	@Override
 	public void processItem(UUID name, Writer output) {
@@ -98,6 +110,7 @@ public class TaskHTMLReporter<USERID> extends CatalogURIReporter<UUID> {
 	};
 	@Override
 	public void footer(Writer output, Iterator<UUID> query) {
+		if (headless) return;
 		try {
 			output.write("</table>");
 			//output.write("<form name=\"myForm\"><input type=BUTTON value=\"Stop polling\" onClick=\"stopPolling()\"></form>");
