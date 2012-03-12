6d46a3c6235ff3e6a3ac5d3b80ced82dd20182c7
ngn <jeliazkova.nina@gmail.com>
2012-03-13 00:05:05 +0200

    User roles test resource

--- restnet-db/src/main/java/net/idea/restnet/db/aalocal/UserRolesResource.java
+++ restnet-db/src/main/java/net/idea/restnet/db/aalocal/UserRolesResource.java
@@ -1,5 +1,6 @@
 package net.idea.restnet.db.aalocal;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -8,30 +9,37 @@ import net.idea.restnet.c.resource.CatalogResource;
 import org.restlet.Context;
 import org.restlet.Request;
 import org.restlet.Response;
-import org.restlet.resource.Get;
+import org.restlet.representation.Representation;
+import org.restlet.representation.Variant;
 import org.restlet.resource.ResourceException;
 import org.restlet.security.Role;
 import org.restlet.security.User;
 
-public class UserRolesResource extends CatalogResource {
+public class UserRolesResource extends CatalogResource<String> {
 
     /** Current user. */
     User user;
 
     /** Its role(s). */
-    List<Role> roles;
-
-    @Override
-    protected void doInit() throws ResourceException {
-        this.user = getClientInfo().getUser();
-        this.roles = getClientInfo().getRoles();
-    }
-
+    List<String> messages = new ArrayList<String>();
   
 	@Override
-	protected Iterator<Role> createQuery(Context context, Request request,
+	protected Iterator<String> createQuery(Context context, Request request,
 			Response response) throws ResourceException {
-		return roles.iterator();
+    	try {
+	        this.user = getClientInfo().getUser();
+	        if (user==null) messages.add("Not logged in user");
+	        else messages.add(user.getIdentifier());
+	        if (getClientInfo().getRoles()==null)
+	        	 messages.add("No roles");
+	        else
+	        for (Role role : getClientInfo().getRoles())
+	        	messages.add(role.toString());
+    	} catch (Exception x) {
+    		messages.add(x.getMessage());
+    	}		
+		return messages.iterator();
 	}
 
+	
 }
