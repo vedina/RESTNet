5efd46f3b2284e575ee5d23de4d3b8200a0b5fb6
ngn <jeliazkova.nina@gmail.com>
2012-11-06 22:22:09 +0200

    DBname prefix for both tables

--- restnet-db/src/main/java/net/idea/restnet/db/aalocal/user/UserAuth.java
+++ restnet-db/src/main/java/net/idea/restnet/db/aalocal/user/UserAuth.java
@@ -21,7 +21,7 @@ public class UserAuth extends AbstractQuery<String, String, EQCondition, Boolean
 	 * 
 	 */
 	private static final long serialVersionUID = 6017803463536586392L;
-	protected final String sql = "select user_name from %s%susers join user_registration using(user_name) where status='confirmed' and user_name = ? and user_pass = md5(?)";
+	protected final String sql = "select user_name from %s%susers join %s%suser_registration using(user_name) where status='confirmed' and user_name = ? and user_pass = md5(?)";
 	
 	public double calculateMetric(Boolean object) {
 		return 1;
@@ -41,7 +41,10 @@ public class UserAuth extends AbstractQuery<String, String, EQCondition, Boolean
 	}
 
 	public String getSQL() throws AmbitException {
-		return String.format(sql,databaseName==null?"":databaseName,databaseName==null?"":".");		
+		return String.format(sql,
+				databaseName==null?"":databaseName,databaseName==null?"":".",
+				databaseName==null?"":databaseName,databaseName==null?"":"."						
+					);		
 	}
 	/**
 	 * If found, will return true always. 
