1124edcc57a5897f8fbc15e59c5e18b756489d66
ngn <jeliazkova.nina@gmail.com>
2013-01-04 20:16:16 +0200

    comments have to have a space

--- restnet-db/src/main/resources/net/idea/restnet/db/aalocal/sql/users.sql
+++ restnet-db/src/main/resources/net/idea/restnet/db/aalocal/sql/users.sql
@@ -1,6 +1,6 @@
-------------------------------------------
+-- ----------------------------------------
 -- Users
-------------------------------------------
+-- ----------------------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE  `users` (
   `user_name` varchar(16) CHARACTER SET latin1 NOT NULL,
@@ -8,18 +8,18 @@ CREATE TABLE  `users` (
   PRIMARY KEY (`user_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-------------------------------------------
+-- ----------------------------------------
 -- Roles
-------------------------------------------
+-- ----------------------------------------
 DROP TABLE IF EXISTS `roles`;
 CREATE TABLE  `roles` (
   `role_name` varchar(16) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`role_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-------------------------------------------
+-- ----------------------------------------
 -- Roles assigned to users
-------------------------------------------
+-- ----------------------------------------
 
 DROP TABLE IF EXISTS `user_roles`;
 CREATE TABLE  `user_roles` (
@@ -28,9 +28,9 @@ CREATE TABLE  `user_roles` (
   PRIMARY KEY (`user_name`,`role_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-------------------------------------------
+-- ----------------------------------------
 -- Registration status and confirmation codes
-------------------------------------------
+-- ----------------------------------------
 DROP TABLE IF EXISTS `user_registration`;
 CREATE TABLE  `user_registration` (
   `user_name` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
@@ -42,9 +42,9 @@ CREATE TABLE  `user_registration` (
   UNIQUE KEY `Index_2` (`code`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-------------------------------------------
+-- ----------------------------------------
 -- Version
-------------------------------------------
+-- ----------------------------------------
 CREATE TABLE  `version` (
   `idmajor` int(5) unsigned NOT NULL,
   `idminor` int(5) unsigned NOT NULL,
