2855ba75b605240d53e0916dc8c7ffe8b3402ce3
ngn <jeliazkova.nina@gmail.com>
2015-07-28 13:26:06 +0300

    code formatting

--- restnet-db/src/main/java/net/idea/restnet/db/convertors/AbstractObjectConvertor.java
+++ restnet-db/src/main/java/net/idea/restnet/db/convertors/AbstractObjectConvertor.java
@@ -14,41 +14,43 @@ import org.restlet.representation.Representation;
  * @param <T>
  * @param <Q>
  */
-public abstract class AbstractObjectConvertor<T, Q extends IQueryRetrieval<T>, Output> extends
-	QueryRepresentationConvertor<T, Q, Output> {
+public abstract class AbstractObjectConvertor<T, Q extends IQueryRetrieval<T>, Output>
+		extends QueryRepresentationConvertor<T, Q, Output> {
 
-    public AbstractObjectConvertor(QueryReporter<T, Q, Output> reporter) {
-	this(reporter, MediaType.TEXT_PLAIN);
-	if (this.reporter != null)
-	    ((QueryReporter<T, Q, Output>) this.reporter).setMaxRecords(5000);
-    }
+	public AbstractObjectConvertor(QueryReporter<T, Q, Output> reporter) {
+		this(reporter, MediaType.TEXT_PLAIN);
+		if (this.reporter != null)
+			((QueryReporter<T, Q, Output>) this.reporter).setMaxRecords(5000);
+	}
 
-    public AbstractObjectConvertor(QueryReporter<T, Q, Output> reporter, MediaType media, String fileNamePrefix) {
-	super(reporter, media, fileNamePrefix);
-    }
+	public AbstractObjectConvertor(QueryReporter<T, Q, Output> reporter,
+			MediaType media, String fileNamePrefix) {
+		super(reporter, media, fileNamePrefix);
+	}
 
-    public AbstractObjectConvertor(QueryReporter<T, Q, Output> reporter, MediaType media) {
-	this(reporter, media, null);
-    }
+	public AbstractObjectConvertor(QueryReporter<T, Q, Output> reporter,
+			MediaType media) {
+		this(reporter, media, null);
+	}
 
-    /**
+	/**
 	 * 
 	 */
-    private static final long serialVersionUID = 6126693410309179856L;
+	private static final long serialVersionUID = 6126693410309179856L;
 
-    public abstract Representation processDoc(Output doc) throws AmbitException;
+	public abstract Representation processDoc(Output doc) throws AmbitException;
 
-    protected abstract Output createOutput(Q query) throws AmbitException;
+	protected abstract Output createOutput(Q query) throws AmbitException;
 
-    @Override
-    public Representation process(Q query) throws Exception {
-	reporter.setOutput(createOutput(query));
-	Representation r = processDoc(reporter.process(query));
-	try {
-	    reporter.close();
-	} catch (Exception x) {
-	}
-	return r;
-    };
+	@Override
+	public Representation process(Q query) throws Exception {
+		reporter.setOutput(createOutput(query));
+		Representation r = processDoc(reporter.process(query));
+		try {
+			reporter.close();
+		} catch (Exception x) {
+		}
+		return r;
+	};
 
 }
--- restnet-db/src/main/java/net/idea/restnet/db/convertors/QueryRepresentationConvertor.java
+++ restnet-db/src/main/java/net/idea/restnet/db/convertors/QueryRepresentationConvertor.java
@@ -18,35 +18,40 @@ import org.restlet.representation.Representation;
  * @param <Q>
  * @param <Output>
  */
-public abstract class QueryRepresentationConvertor<T, Q extends IQueryRetrieval<T>, Output> extends
-	RepresentationConvertor<T, Q, Output, QueryAbstractReporter<T, Q, Output>> {
+public abstract class QueryRepresentationConvertor<T, Q extends IQueryRetrieval<T>, Output>
+		extends
+		RepresentationConvertor<T, Q, Output, QueryAbstractReporter<T, Q, Output>> {
 
-    /**
+	/**
 	 * 
 	 */
-    private static final long serialVersionUID = -2458607015810762273L;
+	private static final long serialVersionUID = -2458607015810762273L;
 
-    public QueryRepresentationConvertor(QueryAbstractReporter<T, Q, Output> reporter) {
-	this(reporter, MediaType.TEXT_PLAIN, null);
-    }
+	public QueryRepresentationConvertor(
+			QueryAbstractReporter<T, Q, Output> reporter) {
+		this(reporter, MediaType.TEXT_PLAIN, null);
+	}
 
-    public QueryRepresentationConvertor(QueryAbstractReporter<T, Q, Output> reporter, MediaType media) {
-	this(reporter, media, null);
-    }
+	public QueryRepresentationConvertor(
+			QueryAbstractReporter<T, Q, Output> reporter, MediaType media) {
+		this(reporter, media, null);
+	}
 
-    public QueryRepresentationConvertor(QueryAbstractReporter<T, Q, Output> reporter, MediaType media,
-	    String fileNamePrefix) {
-	super(reporter, media, fileNamePrefix);
-	if (this.reporter != null)
-	    ((QueryAbstractReporter<T, Q, Output>) this.reporter).setMaxRecords(5000);
-    }
+	public QueryRepresentationConvertor(
+			QueryAbstractReporter<T, Q, Output> reporter, MediaType media,
+			String fileNamePrefix) {
+		super(reporter, media, fileNamePrefix);
+		if (this.reporter != null)
+			((QueryAbstractReporter<T, Q, Output>) this.reporter)
+					.setMaxRecords(5000);
+	}
 
-    @Override
-    public abstract Representation process(Q query) throws Exception;
+	@Override
+	public abstract Representation process(Q query) throws Exception;
 
-    @Override
-    public QueryAbstractReporter<T, Q, Output> getReporter() {
-	return super.getReporter();
-    }
+	@Override
+	public QueryAbstractReporter<T, Q, Output> getReporter() {
+		return super.getReporter();
+	}
 
 }
