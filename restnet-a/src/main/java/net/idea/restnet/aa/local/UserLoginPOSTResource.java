390df8009a5af9120e1a4e50cfeeec7224699da0
ngn <jeliazkova.nina@gmail.com>
2015-10-10 13:03:29 +0300

    make use of the redirect parameter

--- restnet-a/src/main/java/net/idea/restnet/aa/local/UserLoginPOSTResource.java
+++ restnet-a/src/main/java/net/idea/restnet/aa/local/UserLoginPOSTResource.java
@@ -15,7 +15,9 @@ import net.idea.restnet.c.resource.CatalogResource;
 import org.restlet.Context;
 import org.restlet.Request;
 import org.restlet.Response;
+import org.restlet.data.Form;
 import org.restlet.data.MediaType;
+import org.restlet.data.Reference;
 import org.restlet.data.Status;
 import org.restlet.representation.Representation;
 import org.restlet.representation.Variant;
@@ -23,75 +25,96 @@ import org.restlet.resource.ResourceException;
 import org.restlet.security.User;
 
 public class UserLoginPOSTResource<U extends User> extends CatalogResource<U> {
-    public static final String resource = "signin";
+	public static final String resource = "signin";
 
-    @Override
-    protected HTMLBeauty getHTMLBeauty() {
-	return new HTMLBeauty();
-    }
-
-    /**
-     * Intercepted by {@link CookieAuthenticator}
-     */
-    @Override
-    protected Representation post(Representation entity, Variant variant) throws ResourceException {
-
-	if (getRequest().getChallengeResponse() != null) {
-	    /*
-	     * CookieSetting cS = new CookieSetting(0,
-	     * "Credentials",getRequest().getChallengeResponse().getRawValue());
-	     * cS.setComment("CookieAuthenticator"); cS.setPath("/");
-	     * this.getResponse().setStatus(Status.SUCCESS_OK);
-	     * this.getResponse().getCookieSettings().removeAll("Credentials");
-	     * this.getResponse().getCookieSettings().add(cS);
-	     * this.getRequest().getCookies().add(
-	     * "Credentials",getRequest().getChallengeResponse().getRawValue());
-	     */
+	@Override
+	protected HTMLBeauty getHTMLBeauty() {
+		return new HTMLBeauty();
 	}
-	this.getResponse().redirectSeeOther(String.format("%s/login", getRequest().getRootRef()));
-	return null;
-    }
 
-    @Override
-    public IProcessor<Iterator<U>, Representation> createConvertor(Variant variant) throws AmbitException,
-	    ResourceException {
+	/**
+	 * Intercepted by {@link CookieAuthenticator}
+	 */
+	@Override
+	protected Representation post(Representation entity, Variant variant)
+			throws ResourceException {
 
-	if (variant.getMediaType().equals(MediaType.TEXT_HTML)) {
-	    return new StringConvertor(createHTMLReporter(false), MediaType.TEXT_HTML);
-	} else if (variant.getMediaType().equals(MediaType.TEXT_URI_LIST)) {
-	    return new StringConvertor(new UserLoginURIReporter(getRequest(), getDocumentation()) {
-		@Override
-		public void processItem(Object item, Writer output) {
-		    super.processItem(item, output);
-		    try {
-			output.write('\n');
-		    } catch (Exception x) {
-		    }
+		if (getRequest().getChallengeResponse() != null) {
+			/*
+			 * CookieSetting cS = new CookieSetting(0,
+			 * "Credentials",getRequest().getChallengeResponse().getRawValue());
+			 * cS.setComment("CookieAuthenticator"); cS.setPath("/");
+			 * this.getResponse().setStatus(Status.SUCCESS_OK);
+			 * this.getResponse().getCookieSettings().removeAll("Credentials");
+			 * this.getResponse().getCookieSettings().add(cS);
+			 * this.getRequest().getCookies().add(
+			 * "Credentials",getRequest().getChallengeResponse().getRawValue());
+			 */
 		}
-	    }, MediaType.TEXT_URI_LIST);
+		String redirect = null;
 
-	} else
-	    // html
-	    return new StringConvertor(createHTMLReporter(false), MediaType.TEXT_HTML);
+		if (entity.isAvailable())
+			try {
+				Form form = new Form(entity);
+				redirect = form.getFirstValue("targetUri", true);
+				System.out.println(redirect);
+			} catch (Exception x) {
+				redirect = null;
+			}
+		if (redirect == null)
+			this.getResponse().redirectSeeOther(
+					String.format("%s/login", getRequest().getRootRef()));
+		else
+			this.getResponse().redirectSeeOther(redirect);
 
-    }
-
-    @Override
-    protected Reporter createHTMLReporter(boolean headles) {
-	return new UserLoginHTMLReporter(getRequest(), getDocumentation(), getHTMLBeauty());
-    }
-
-    @Override
-    protected Iterator<U> createQuery(Context context, Request request, Response response) throws ResourceException {
-
-	User user = request.getClientInfo().getUser();
-	if (user == null) {
-	    user = new User();
+		return null;
 	}
-	if (user instanceof User)
-	    return new SingleItemIterator(user);
-	else
-	    throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST);
-    }
+
+	@Override
+	public IProcessor<Iterator<U>, Representation> createConvertor(
+			Variant variant) throws AmbitException, ResourceException {
+
+		if (variant.getMediaType().equals(MediaType.TEXT_HTML)) {
+			return new StringConvertor(createHTMLReporter(false),
+					MediaType.TEXT_HTML);
+		} else if (variant.getMediaType().equals(MediaType.TEXT_URI_LIST)) {
+			return new StringConvertor(new UserLoginURIReporter(getRequest(),
+					getDocumentation()) {
+				@Override
+				public void processItem(Object item, Writer output) {
+					super.processItem(item, output);
+					try {
+						output.write('\n');
+					} catch (Exception x) {
+					}
+				}
+			}, MediaType.TEXT_URI_LIST);
+
+		} else
+			// html
+			return new StringConvertor(createHTMLReporter(false),
+					MediaType.TEXT_HTML);
+
+	}
+
+	@Override
+	protected Reporter createHTMLReporter(boolean headles) {
+		return new UserLoginHTMLReporter(getRequest(), getDocumentation(),
+				getHTMLBeauty());
+	}
+
+	@Override
+	protected Iterator<U> createQuery(Context context, Request request,
+			Response response) throws ResourceException {
+
+		User user = request.getClientInfo().getUser();
+		if (user == null) {
+			user = new User();
+		}
+		if (user instanceof User)
+			return new SingleItemIterator(user);
+		else
+			throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST);
+	}
 
 }
