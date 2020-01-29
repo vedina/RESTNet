11075e1d71a60e48f37badaf5a9655a4d9ff3607
vedina <jeliazkova.nina@gmail.com>
2020-01-29 14:43:22 +0200

    convertor to throw Exception when the reporter is null T608#17297 NPE-01:

--- restnet-c/src/main/java/net/idea/restnet/c/StringConvertor.java
+++ restnet-c/src/main/java/net/idea/restnet/c/StringConvertor.java
@@ -4,14 +4,16 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 
-import net.idea.modbcum.i.exceptions.AmbitException;
-import net.idea.modbcum.i.reporter.Reporter;
-
 import org.restlet.data.CharacterSet;
 import org.restlet.data.Language;
 import org.restlet.data.MediaType;
+import org.restlet.data.Status;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.ResourceException;
+
+import net.idea.modbcum.i.exceptions.AmbitException;
+import net.idea.modbcum.i.reporter.Reporter;
 
 /**
  * Converts query results to string
@@ -24,42 +26,47 @@ import org.restlet.representation.StringRepresentation;
  */
 public class StringConvertor<T, Q, R extends Reporter<Q, Writer>> extends RepresentationConvertor<T, Q, Writer, R> {
 
-    /**
+	/**
 	 * 
 	 */
-    private static final long serialVersionUID = 6126693410309179856L;
+	private static final long serialVersionUID = 6126693410309179856L;
 
-    public StringConvertor(R reporter, MediaType mediaType) {
-	this(reporter, mediaType, null);
-    }
-
-    public StringConvertor(R reporter, MediaType mediaType, String fileNamePrefix) {
-	super(reporter, mediaType, fileNamePrefix);
-    }
-
-    @Override
-    public Representation process(Q query) throws Exception {
-	try {
-	    reporter.setOutput(new StringWriter());
-	    Writer writer = reporter.process(query);
-	    writer.flush();
-	    StringRepresentation rep = new StringRepresentation(writer.toString(), getMediaType(), Language.ENGLISH,
-		    CharacterSet.UTF_8);
-	    setDisposition(rep);
-	    return rep;
-	} catch (IOException x) {
-	    throw new AmbitException(x);
-	} finally {
-	    try {
-		reporter.close();
-	    } catch (Exception x) {
-	    }
+	public StringConvertor(R reporter, MediaType mediaType) throws ResourceException {
+		this(reporter, mediaType, null);
 	}
 
-    };
+	public StringConvertor(R reporter, MediaType mediaType, String fileNamePrefix) throws ResourceException {
+		super(reporter, mediaType, fileNamePrefix);
+		if (reporter == null) throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST);
+	}
 
-    @Override
-    public void open() throws Exception {
+	@Override
+	public Representation process(Q query) throws Exception {
+		if (reporter == null)
+			throw new AmbitException("Reporter is null");
+		if (query == null)
+			throw new AmbitException("Query is null");
+		try {
+			reporter.setOutput(new StringWriter());
+			Writer writer = reporter.process(query);
+			writer.flush();
+			StringRepresentation rep = new StringRepresentation(writer.toString(), getMediaType(), Language.ENGLISH,
+					CharacterSet.UTF_8);
+			setDisposition(rep);
+			return rep;
+		} catch (IOException x) {
+			throw new AmbitException(x);
+		} finally {
+			try {
+				reporter.close();
+			} catch (Exception x) {
+			}
+		}
 
-    }
+	};
+
+	@Override
+	public void open() throws Exception {
+
+	}
 }
